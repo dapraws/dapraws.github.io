fba5f8bb3b182d58a8a83fd1a2125ab97434452d
Muhammad Darrel Prawira <122019775+dapraws@users.noreply.github.com>
2023-12-16 16:28:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,34 @@ function displayMenu() {
         iconClose.style.display='inline';
     }
 }
+
+// Get all images inside the 'item' class
+const images = document.querySelectorAll('.item img');
+
+// Get the popup and its content
+const popup = document.getElementById('popup');
+const popupImg = document.getElementById('popup-img');
+
+// Loop through each image and add a click event listener
+images.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        // Display the popup
+        popup.style.display = 'flex';
+        
+        // Set the clicked image source to the popup image
+        popupImg.src = img.src;
+        popupImg.alt = img.alt;
+    });
+});
+
+// Close popup when close button is clicked
+document.getElementById('close').addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+// Close popup when clicking outside the image
+popup.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
